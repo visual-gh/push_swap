16b0fca1334818a4f495082e3bb4f3d8a82377c9
visual-gh <github.molecular850@passmail.net>
2026-02-04 21:16:44 +0000

    Parser

---
+++ srcs/parser/parse.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Visual <github.com/visual-gh>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 20:33:18 by Visual            #+#    #+#             */
+/*   Updated: 2026/02/04 21:04:54 by Visual           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	add_number(t_stack **stack, char *nbr_str)
+{
+	t_stack	*new;
+	int		nbr;
+
+	if (!is_valid_int(nbr_str))
+		return (1);
+	nbr = (int)ft_atol(nbr_str);
+	new = stack_new(nbr);
+	if (!new)
+		return (1);
+	stack_add_back(stack, new);
+	return (0);
+}
+
+static int	parse_quoted_args(t_stack **stack, char *arg)
+{
+	char	**str;
+	int		i;
+
+	str = ft_split(arg, ' ');
+	if (!str)
+		return (1);
+	i = 0;
+	while (str[i])
+	{
+		if (add_number(stack, str[i]))
+			return (free(str), 1);
+		i++;
+	}
+	return (free(str), 0);
+}
+
+int	parse_args(char **av, t_stack **stack_a)
+{
+	int	i;
+
+	i = 1;
+	if (!av[1])
+		return (1);
+	while (av[i])
+	{
+		if (ft_strchr(av[i], ' '))
+			if (parse_quoted_args(stack_a, av[i]))
+				return (1);
+		else
+			if (add_number(stack_a, av[i]))
+				return (1);
+		i++;
+	}
+	if (has_duplicates(*stack_a))
+		return (1);
+	return (0);
+}
---
+++ srcs/parser/utils.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Visual <github.com/visual-gh>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 19:56:34 by Visual            #+#    #+#             */
+/*   Updated: 2026/02/04 20:19:11 by Visual           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+long	ft_atol(const char *str)
+{
+	long	nbr;
+	int		sign;
+
+	nbr = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+		sign = 1 - (2 * (*str++ == '-'));
+	while (*str >= '0' && *str <= '9')
+		nbr = nbr * 10 + (*str++ - '0');
+	return (nbr * sign);
+}
+
+int	is_valid_int(char *str)
+{
+	long	nbr;
+	int		i;
+
+	i = 0;
+	if (!str || !str[0])
+		return (0);
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i++])
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	nbr = ft_atol(str);
+	if (nbr < -2147483648 || num > 2147483647)
+		return (0);
+	return (1);
+}
