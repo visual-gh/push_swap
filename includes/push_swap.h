8afeb815b09e8b7aa704bdac82cfd412d6cea3f7
visual-gh <github.molecular850@passmail.net>
2026-02-15 13:41:19 +0000

    Comments

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: Visual <github.com/visual-gh>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 16:56:30 by Visual            #+#    #+#             */
-/*   Updated: 2026/02/09 04:02:33 by Visual           ###   ########.fr       */
+/*   Updated: 2026/02/15 13:40:43 by Visual           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <limits.h>
 # include "libft.h"
 
-/* Stack Structure */
+// Stack Structure
 
 typedef struct s_stack
 {
@@ -27,14 +27,14 @@ typedef struct s_stack
 	struct s_stack	*next;
 }	t_stack;
 
-/* Parsing & Validation */
+// Parsing & Validation
 
 long		ft_atol(const char *str);
 int			has_duplicates(t_stack *stack);
 int			is_valid_int(char *str);
 int			parse_args(char **argv, t_stack **a);
 
-/* Stack Data Structure */
+// Stack Data Structure
 
 t_stack		*stack_new(int value);
 void		stack_add_back(t_stack **stack, t_stack *new);
@@ -42,30 +42,30 @@ int			stack_size(t_stack *stack);
 t_stack		*stack_last(t_stack *stack);
 void		stack_clear(t_stack **stack);
 
-/* Stack Operations - Swap */
+// Stack Operations - Swap
 
 void		sa(t_stack **a, int print);
 void		sb(t_stack **b, int print);
 void		ss(t_stack **a, t_stack **b, int print);
 
-/* Stack Operations - Push */
+// Stack Operations - Push
 
 void		pa(t_stack **a, t_stack **b, int print);
 void		pb(t_stack **a, t_stack **b, int print);
 
-/* Stack Operations - Rotate */
+// Stack Operations - Rotate
 
 void		ra(t_stack **a, int print);
 void		rb(t_stack **b, int print);
 void		rr(t_stack **a, t_stack **b, int print);
 
-/* Stack Operations - Reverse Rotate */
+// Stack Operations - Reverse Rotate
 
 void		rra(t_stack **a, int print);
 void		rrb(t_stack **b, int print);
 void		rrr(t_stack **a, t_stack **b, int print);
 
-/* Algorithm */
+// Algorithms
 
 void		index_stack(t_stack **stack);
 int			is_sorted(t_stack *stack);
